d0b860ed61ea3aa617cc3ec20cb609ed6b426b9e
monkey1221 <monkey1221@git.co.kr>
2016-07-06 10:10:12 +0900

    지역 검색 Activity 추가

---
+++ Arbiter-Android-master/Arbiter-Android/src/com/lmn/Arbiter_Android/Activities/FindAreaActivity.java
@@ -0,0 +1,109 @@
+package com.lmn.Arbiter_Android.Activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.lmn.Arbiter_Android.R;
+
+import org.apache.cordova.Config;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+
+/**
+ * Created by pc on 2016-05-01.
+ */
+public class FindAreaActivity extends Activity  {
+
+    Geocoder mCoder;
+    static double lat,lon;
+    EditText address;
+    Button ok;
+    ListView addressListView;
+    ArrayAdapter <String> adapter;
+    List<Address> location;
+    List<Address> addressResult = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Config.init(this);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.find_area);
+
+        mCoder = new Geocoder(this,getResources().getConfiguration().locale);
+
+        address = (EditText) findViewById(R.id.address);
+        ok = (Button) findViewById(R.id.find);
+        addressListView = (ListView) findViewById(R.id.addressList);
+        adapter = new ArrayAdapter (this, android.R.layout.simple_list_item_1);
+        addressListView.setAdapter(adapter);
+        ok.setOnClickListener(addressListener);
+        addressListView.setOnItemClickListener(listViewExampleClickListener);
+
+    }
+    AdapterView.OnItemClickListener listViewExampleClickListener = new AdapterView.OnItemClickListener() {
+        public void onItemClick(AdapterView<?> parentView, View clickedView, int position, long id) {
+            Intent intent = new Intent();
+            double lat = location.get(position).getLatitude();
+            double lon = location.get(position).getLongitude();
+            String latlon = lat+","+lon;
+            intent.putExtra("location", latlon);
+            setResult(101, intent);
+            finish();
+        }
+    };
+    View.OnClickListener addressListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            adapter.clear();
+            String saddr = address.getText().toString();
+
+            try{
+                location = mCoder.getFromLocationName(saddr,5);
+
+            }catch (IOException e)
+            {
+                Log.d("IO error : ", e.getMessage());
+                return;
+            }
+
+            if (location.size() == 0)
+            {
+                Log.d("no result","no result");
+                return;
+            }
+            else
+            {
+                for (int i=0; i<location.size(); i++)
+                {
+                    Address addr = location.get(i);
+                    int MaxIndex = addr.getMaxAddressLineIndex();
+                    String ad="";
+                    if (MaxIndex == 0)
+                        ad = addr.getAddressLine(0);
+                    else
+                    {
+                        for (int j=0; j<MaxIndex; j++)
+                            ad = ad + addr.getAddressLine(j) + " ";
+
+                    }
+                    adapter.add(ad);
+                }
+            }
+            adapter.notifyDataSetChanged();
+        }
+    };
+}
